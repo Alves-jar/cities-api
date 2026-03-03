f6b9eb0c6b47d66f7623ab7518ab8e8a1d199343
AlvesKZ <igoralvex01@gmail.com>
2026-03-03 12:28:05 -0300

    feat: add delete cities provider

--- src/server/database/providers/cidades/GetAll.ts
+++ src/server/database/providers/cidades/GetAll.ts
@@ -1,18 +1,29 @@
 import { ETableNames } from '../../ETableNames';
-import { Knex } from '../../knex';
 import { ICidade } from '../../models';
+import { Knex } from '../../knex';
 
-export const getAll = async (): Promise<ICidade[] | Error> => {
+
+export const getAll = async (page: number, limit: number, filter: string, id = 0): Promise<ICidade[] | Error> => {
     try {
-        const result = await Knex(ETableNames.cidade).select('*');
+        const result = await Knex(ETableNames.cidade)
+            .select('*')
+            .where('id', Number(id))
+            .orWhere('nome', 'like', `%${filter}%`)
+            .offset((page - 1) * limit)
+            .limit(limit);
 
-        if (result.length > 0) {
-            return result;
+        if (id > 0 && result.every(item => item.id !== id)) {
+            const resultById = await Knex(ETableNames.cidade)
+                .select('*')
+                .where('id', '=', id)
+                .first();
+
+            if (resultById) return [...result, resultById];
         }
 
-        return [];
+        return result;
     } catch (error) {
         console.log(error);
-        return new Error('Error to get cities.');
+        return new Error('Erro ao consultar os registros');
     }
 };
--- src/server/database/providers/cidades/GetById.ts
+++ src/server/database/providers/cidades/GetById.ts
@@ -1,21 +1,20 @@
 import { ETableNames } from '../../ETableNames';
-import { Knex } from '../../knex';
 import { ICidade } from '../../models';
+import { Knex } from '../../knex';
+
 
 export const getById = async (id: number): Promise<ICidade | Error> => {
     try {
         const result = await Knex(ETableNames.cidade)
             .select('*')
-            .where('id', id)
+            .where('id', '=', id)
             .first();
 
-        if (result) {
-            return result;
-        }
+        if (result) return result;
 
-        return new Error('City not found.');
+        return new Error('Registro não encontrado');
     } catch (error) {
         console.log(error);
-        return new Error('Error to get the city.');
+        return new Error('Erro ao consultar o registro');
     }
 };
