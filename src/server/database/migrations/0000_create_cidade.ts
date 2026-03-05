ea7c8dff818fe3a2ab82df17bec2862d80294454
AlvesKZ <igoralvex01@gmail.com>
2026-03-05 17:57:10 -0300

    feat: create people table

--- src/server/database/migrations/0000_create_cidade.ts
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -1,17 +1,17 @@
-import { Knex } from "knex";
-import { ETableNames } from "../ETableNames";
+import { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
 
 export async function up(knex: Knex) {
     return knex.schema
         .createTable(ETableNames.cidade, (table) => {
-            table.bigIncrements("id").primary().index();
+            table.bigIncrements('id').primary().index();
             table
-                .string("nome", 150)
-                .checkLength("<=", 150)
+                .string('nome', 150)
+                .checkLength('<=', 150)
                 .index()
                 .notNullable();
 
-            table.comment("Tabela usada para armazenar cidades do sistema");
+            table.comment('Tabela usada para armazenar cidades do sistema');
         })
         .then(() => {
             console.log(`# Create table ${ETableNames.cidade}`);
